9eb0dbe03fb2ad9ba096ca2fd3cd52c779c98183
Adam22 <ad.tumi@gmail.com>
2016-02-18 23:21:02 +0100

    search UI form rewrited

--- javascripts/korbanek-map.js
+++ javascripts/korbanek-map.js
@@ -1,8 +1,6 @@
 (function ( $j ) {
     //Declare no-conflict
     $j = jQuery.noConflict();
-    //Strict mode
-    'use strict';
     $j(document).ready(function(){
         $j('div[data-' + defaults.mapSettingsDataAttr + ']').each(function(){            
             $j(this).GoogleMapPlugin();
@@ -65,98 +63,33 @@
     };
 
     //Plugin Definition
-    $j.fn.GoogleMapPlugin = function(){
-        var self = this;
-        //Retrive Data
+    $j.fn.GoogleMapPlugin = function(){               
+        //Retrive Data From Html
         var options = $j(this).data(defaults.mapSettingsDataAttr);
         //Container ID
         options['onContainer'] = $j(this).attr('id');  
         //Info Window Open event
-        options['openInfoWindowOn'] = $j.fn.GoogleMapPlugin.prototype.obtainOnClickEvent();
+        options['openInfoWindowOn'] = null;
         //Start Search On event
-        options['startSearchOn'] = $j.fn.GoogleMapPlugin.prototype.obtainOnClickEvent();
-        //Detect User Position from Browser        
-        if(options.detectUserPosition){
-            $j.fn.GoogleMapPlugin.prototype.retriveUserPosition(options, self, function(){
-                //Compose Settings Object
-                var mapOptions = $j.extend({}, defaults, options);
-                //Start jQuery Plugin Chain
-                $j(this).GoogleMapPlugin.searchFeatureUI(mapOptions, $j(self)).createMap(mapOptions);
-            });
-        }else{
-            //Compose Settings Object
-            var mapOptions = $j.extend({}, defaults, options);
-            //Start jQuery Plugin Chain
-            $j(this).GoogleMapPlugin.searchFeatureUI(mapOptions, $j(self)).createMap(mapOptions);            
-        }
+        options['startSearchOn'] = null;
+        //Detect User Position from Browser
+        var mapOptions = $j.extend({}, defaults, options);
+        var that = this;
+        $j(this).GoogleMapPlugin.searchFeatureUI(mapOptions, that)
     };
     
     //Insert Search Field
     $j.fn.GoogleMapPlugin.searchFeatureUI = function(mapOptions, element){
         if(mapOptions.searchFeature){
             $j(element).parent().before(
-                '<div class="form-group">\n\
-                    <label for="' + mapOptions.bindSearchFeatureTo + '">' + mapOptions.inputLabel + '</label>\n\
-                    <input type="text" class="form-control" id="address" placeholder="' + mapOptions.inputText + '">\n\
-                    <input class="btn btn-default" type="submit" id="' + mapOptions.bindSearchFeatureTo + '" value="' + mapOptions.searchButtonText + '">\n\
-                </div>');
+                '<form role="form">\n\
+                    <div class="form-group">\n\
+                        <label for="' + mapOptions.bindSearchFeatureTo + '">' + mapOptions.inputLabel + '</label>\n\
+                        <input type="text" class="form-control" id="address" placeholder="' + mapOptions.inputText + '">\n\
+                        <button id="' + mapOptions.bindSearchFeatureTo + ' type="submit" class="btn btn-default">' + mapOptions.searchButtonText + '</button>\n\                        \n\
+                    </div>\n\
+                </form>');
         }
         return this;
     };
-    
-    $j.fn.GoogleMapPlugin.createMap = function(options){        
-        //Apply GoogleMap on source element
-        this.korbanekMap = new KorbanekMap(options);        
-        this.korbanekMap.embedMap();        
-        this.korbanekMap.mapResize(this.korbanekMap.map);
-        //Define default markers set
-        if (this.korbanekMap.config.showAll){
-            this.korbanekMap.config.defaultMarkerSet = this.korbanekMap.markerSet;
-        }else{
-            this.korbanekMap.config.defaultMarkerSet = this.korbanekMap.centralMarker;
-        }
-        //Put markers on the map
-        this.korbanekMap.setupMarkersOnMap(this.korbanekMap.config.defaultMarkerSet, this.korbanekMap.map);
-        
-        return this;
-    };
-    
-    $j.fn.GoogleMapPlugin.prototype.obtainOnClickEvent = function(){        
-        var event = navigator.userAgent.match(/iphone|ipad/gi)
-                ? "touchstart" 
-                : "click";
-        return event;
-    };   
-    
-    $j.fn.GoogleMapPlugin.prototype.detectUserPosition = function(callback, navigatorOptions){   
-        if (navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(function(position){
-                var pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-                if (typeof callback !== 'function') {
-                    callback = false;
-                }
-                else{                            
-                    callback(pos);
-                }
-            }, function(err){
-                console.warn('ERROR(' + err.code + '): ' + err.message);
-                callback();
-            }, navigatorOptions);                                                
-        }
-    };
-        
-    $j.fn.GoogleMapPlugin.prototype.retriveUserPosition = function(options, self, callback){
-        this.detectUserPosition(function(pos){
-            if(pos){
-                options['mapZoom'] = 9;
-                options['mapPosition'] = pos;
-            }
-            callback(self);
-        }, defaults.navigatorOptions);        
-    };   
-    
-  
 }( jQuery ));
